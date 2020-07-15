25af9a5ea6efd528589fe60ea804bd24a9fa2089
Roman Tabunnikov <roman.tabunnikov@devexpress.com>
2020-07-15 16:36:21 +0300

    Try to add point colors

--- Core/Treemap.cs
+++ Core/Treemap.cs
@@ -12,39 +12,54 @@ namespace ChartExModelSpike {
             chart.GroupDataMember = "Meal";
             chart.LabelDataMember = "Product";
             chart.ValueDataMember = "Amount";
-            chart.DataSource = TreemapData.GetSampleData();
+            var data = TreemapData.GetSampleData();
+            chart.DataSource = data;
+            chart.BorderColor = new ChartsModel.ColorARGB(255, 255, 255, 255);
+            chart.ItemBorderVisible = true;
+            chart.Palette = new ChartsModel.Palette(chart);
+            foreach (var item in data) {
+                byte r = (byte)((item.Color & 0x00ff0000) >> 16);
+                byte g = (byte)((item.Color & 0x0000ff00) >> 8);
+                byte b = (byte)(item.Color & 0x000000ff);
+                chart.Palette.Entries.Add(new ChartsModel.PaletteEntry(new ChartsModel.ColorARGB(0xff, r, g, b)));
+            }
             return chart;
         }
     }
 
     public class TreemapData {
+        private const int Color1 = 0x4472c4;
+        private const int Color2 = 0xed7d31;
 
-        public TreemapData(string meal, string category, string product, double amount) {
+        public TreemapData(string meal, string category, string product, double amount, int color) {
             Meal = meal;
             Category = category;
             Product = product;
             Amount = amount;
+            Color = color;
         }
 
         public string Meal { get; }
         public string Category { get; }
         public string Product { get; }
         public double Amount { get; }
+        public int Color { get; }
 
         public static List<TreemapData> GetSampleData() {
             var result = new List<TreemapData>();
-            result.Add(new TreemapData("Breakfast", "Beverage", "Coffee", 20));
-            result.Add(new TreemapData("Breakfast", "Beverage", "Tea", 9));
-            result.Add(new TreemapData("Breakfast", "Food", "Waffles", 12));
-            result.Add(new TreemapData("Breakfast", "Food", "Pancakes", 35));
-            result.Add(new TreemapData("Breakfast", "Food", "Eggs", 24));
-            result.Add(new TreemapData("Lunch", "Beverage", "Coffee", 10));
-            result.Add(new TreemapData("Lunch", "Beverage", "Iced Tea", 45));
-            result.Add(new TreemapData("Lunch", "Food", "Soup", 16));
-            result.Add(new TreemapData("Lunch", "Food", "Sandwich", 36));
-            result.Add(new TreemapData("Lunch", "Food", "Salad", 70));
-            result.Add(new TreemapData("Lunch", "Food", "Pie", 45));
-            result.Add(new TreemapData("Lunch", "Food", "Cookies", 25));
+            result.Add(new TreemapData("Breakfast", "Beverage", "Coffee", 20, Color1));
+            result.Add(new TreemapData("Breakfast", "Beverage", "Tea", 9, Color1));
+            result.Add(new TreemapData("Breakfast", "Food", "Waffles", 12, Color1));
+            result.Add(new TreemapData("Breakfast", "Food", "Pancakes", 35, Color1));
+            result.Add(new TreemapData("Breakfast", "Food", "Eggs", 24, Color1));
+            result.Add(new TreemapData("Lunch", "Beverage", "Coffee", 10, Color2));
+            result.Add(new TreemapData("Lunch", "Beverage", "Iced Tea", 45, Color2));
+            result.Add(new TreemapData("Lunch", "Food", "Soup", 16, Color2));
+            result.Add(new TreemapData("Lunch", "Food", "Sandwich", 36, Color2));
+            result.Add(new TreemapData("Lunch", "Food", "Salad", 70, Color2));
+            result.Add(new TreemapData("Lunch", "Food", "Pie", 45, Color2));
+            result.Add(new TreemapData("Lunch", "Food", "Cookies", 25, Color2));
+            //result.Sort((x, y) => x.Amount.CompareTo(y.Amount));
             return result;
         }
     }
