aaa33b1756a28968bf16b56ee27c1b872127be30
Roman Tabunnikov <roman.tabunnikov@devexpress.com>
2020-07-20 10:30:10 +0300

    Point colors

--- Core/Treemap.cs
+++ Core/Treemap.cs
@@ -12,6 +12,7 @@ namespace ChartExModelSpike {
             chart.GroupDataMember = "Meal";
             chart.LabelDataMember = "Product";
             chart.ValueDataMember = "Amount";
+            chart.ColorDataMember = "Color";
             var data = TreemapData.GetSampleData();
             chart.DataSource = data;
             chart.BorderColor = new ChartsModel.ColorARGB(255, 255, 255, 255);
@@ -21,13 +22,7 @@ namespace ChartExModelSpike {
             chart.Palette = new ChartsModel.Palette(chart);
             chart.Palette.Entries.Add(new ChartsModel.PaletteEntry(new ChartsModel.ColorARGB(0xff, 0x44, 0x72, 0xc4)));
             chart.Palette.Entries.Add(new ChartsModel.PaletteEntry(new ChartsModel.ColorARGB(0xff, 0xed, 0x7d, 0x31)));
-            chart.Palette.Entries.Add(new ChartsModel.PaletteEntry(new ChartsModel.ColorARGB(0xff, 0xa5, 0xa5, 0xa5)));
-            //foreach (var item in data) {
-            //    byte r = (byte)((item.Color & 0x00ff0000) >> 16);
-            //    byte g = (byte)((item.Color & 0x0000ff00) >> 8);
-            //    byte b = (byte)(item.Color & 0x000000ff);
-            //    chart.Palette.Entries.Add(new ChartsModel.PaletteEntry(new ChartsModel.ColorARGB(0xff, r, g, b)));
-            //}
+            //chart.Palette.Entries.Add(new ChartsModel.PaletteEntry(new ChartsModel.ColorARGB(0xff, 0xa5, 0xa5, 0xa5)));
             return chart;
         }
     }
@@ -37,7 +32,7 @@ namespace ChartExModelSpike {
         private const int Color2 = 0xed7d31;
         private const int Color3 = 0xa5a5a5;
 
-        public TreemapData(string meal, string category, string product, double amount, int color) {
+        public TreemapData(string meal, string category, string product, double amount, int? color = null) {
             Meal = meal;
             Category = category;
             Product = product;
@@ -49,25 +44,25 @@ namespace ChartExModelSpike {
         public string Category { get; }
         public string Product { get; }
         public double Amount { get; }
-        public int Color { get; }
+        public int? Color { get; }
 
         public static List<TreemapData> GetSampleData() {
             var result = new List<TreemapData>();
-            result.Add(new TreemapData("Breakfast", "Beverage", "Coffee", 20, Color1));
-            result.Add(new TreemapData("Breakfast", "Beverage", "Tea", 9, Color1));
-            result.Add(new TreemapData("Breakfast", "Food", "Waffles", 12, Color1));
-            result.Add(new TreemapData("Breakfast", "Food", "Pancakes", 35, Color1));
-            result.Add(new TreemapData("Breakfast", "Food", "Eggs", 24, Color1));
-            result.Add(new TreemapData("Lunch", "Beverage", "Coffee", 10, Color2));
-            result.Add(new TreemapData("Lunch", "Beverage", "Iced Tea", 45, Color2));
-            result.Add(new TreemapData("Lunch", "Food", "Soup", 16, Color2));
-            result.Add(new TreemapData("Lunch", "Food", "Sandwich", 36, Color2));
-            result.Add(new TreemapData("Lunch", "Food", "Salad", 70, Color2));
-            result.Add(new TreemapData("Lunch", "Food", "Pie", 45, Color2));
-            result.Add(new TreemapData("Lunch", "Food", "Cookies", 25, Color2));
-            result.Add(new TreemapData("Supper", "Beverage", "Vine", 75, Color3));
-            result.Add(new TreemapData("Supper", "Food", "Fish", 125, Color3));
-            result.Add(new TreemapData("Supper", "Food", "Meat", 150, Color3));
+            result.Add(new TreemapData("Breakfast", "Beverage", "Coffee", 20));
+            result.Add(new TreemapData("Breakfast", "Beverage", "Tea", 9));
+            result.Add(new TreemapData("Breakfast", "Food", "Waffles", 12));
+            result.Add(new TreemapData("Breakfast", "Food", "Pancakes", 35));
+            result.Add(new TreemapData("Breakfast", "Food", "Eggs", 24));
+            result.Add(new TreemapData("Lunch", "Beverage", "Coffee", 10));
+            result.Add(new TreemapData("Lunch", "Beverage", "Iced Tea", 45, unchecked((int)0xff33aa33)));
+            result.Add(new TreemapData("Lunch", "Food", "Soup", 16));
+            result.Add(new TreemapData("Lunch", "Food", "Sandwich", 36));
+            result.Add(new TreemapData("Lunch", "Food", "Salad", 70));
+            result.Add(new TreemapData("Lunch", "Food", "Pie", 45));
+            result.Add(new TreemapData("Lunch", "Food", "Cookies", 25, unchecked((int)0xff6666aa)));
+            //result.Add(new TreemapData("Supper", "Beverage", "Vine", 75));
+            //result.Add(new TreemapData("Supper", "Food", "Fish", 125));
+            //result.Add(new TreemapData("Supper", "Food", "Meat", 150));
             //result.Sort((x, y) => x.Amount.CompareTo(y.Amount));
             return result;
         }
