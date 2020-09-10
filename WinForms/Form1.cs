e3e22ee754cf3233b8f096f501022e319434c3f9
Anatoly Proydin <anatoly.proydin@devexpress.com>
2020-09-10 12:39:44 +0300

    merge controllers

--- WinForms/Form1.cs
+++ WinForms/Form1.cs
@@ -1,102 +1,103 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using ChartsModel = DevExpress.Charts.Model;
 using DevExpress.XtraCharts.ModelSupport;
 using DevExpress.XtraTreeMap.Native;
+using DevExpress.Charts.Model;
 
 namespace ChartExModelSpike {
-    public partial class Form1 : Form {
-        private readonly XtraChartsModelControllerFactory factory;
-        private ChartsModel.Controller controller = null;
-        private ChartsModel.Chart modelChart = null;
+    public enum ChartType {
+        BoxWhisker = 0,
+        Histogram = 1,
+        Funnel = 2,
+        Pareto = 3,
+        Sunburst = 4,
+        TreeMap = 5,
+        Waterfall = 6
+    }
 
-        private readonly TreeModelControllerFactory treeMapFactory;
-        private TreeMapController treeMapController = null;
-        private ChartsModel.TreeMap treeMapChart = null;
+    public partial class Form1 : Form {
+        private ModelControllerFactoryBase factory;
+        private ChartsModel.IController controller = null;
+        private ChartsModel.ChartBase modelChart = null;
 
         public Form1() {
             InitializeComponent();
-            factory = new XtraChartsModelControllerFactory();
-            controller = factory.CreateController();
-            treeMapFactory = new TreeModelControllerFactory();
-            treeMapController = (TreeMapController)treeMapFactory.CreateController();
         }
 
         private void View_Paint(object sender, PaintEventArgs e) {
-            if(modelChart != null) {
+            if (modelChart != null) {
                 var graphics = e.Graphics;
                 ChartsModel.ModelRect rect = new ChartsModel.ModelRect(8, 8, viewPanel.ClientSize.Width - 16, viewPanel.ClientSize.Height - 16);
                 var renderContext = factory.CreateRenderContext(rect, graphics);
                 controller.RenderChart(renderContext);
             }
-            else if (treeMapChart != null) {
-                var graphics = e.Graphics;
-                ChartsModel.ModelRect rect = new ChartsModel.ModelRect(8, 8, viewPanel.ClientSize.Width - 16, viewPanel.ClientSize.Height - 16);
-                var renderContext = treeMapFactory.CreateRenderContext(rect, graphics);
-                treeMapController.RenderChart(renderContext);
-            }
         }
 
         private void View_SizeChanged(object sender, EventArgs e) => viewPanel.Invalidate();
 
-        private void ResetController() {
+        private void Reset(ChartType chartType) {
             if (modelChart != null) {
                 modelChart = null;
-                controller.ChartModel = null;
-                controller = factory.CreateController();
             }
-            if (treeMapChart != null) {
-                treeMapChart = null;
-                treeMapController.ChartModel = null;
-                treeMapController = (TreeMapController)treeMapFactory.CreateController();
+            factory = CreateFactory(chartType);
+            controller = factory.CreateController();
+        }
+        ModelControllerFactoryBase CreateFactory(ChartType chartType) {
+            switch (chartType) {
+                case ChartType.BoxWhisker:
+                case ChartType.Funnel:
+                case ChartType.Histogram:
+                case ChartType.Pareto:
+                case ChartType.Waterfall:
+                    return new XtraChartsModelControllerFactory();
+                case ChartType.TreeMap:
+                case ChartType.Sunburst:
+                    return new TreeModelControllerFactory();
+                default:
+                    throw new ArgumentException("Unknown chart type.");
             }
         }
 
         private void butWaterfall_Click(object sender, EventArgs e) {
-            ResetController();
+            Reset(ChartType.Waterfall);
             modelChart = Waterfall.Create();
             controller.ChartModel = modelChart;
             viewPanel.Invalidate();
         }
 
         private void butBoxWhisker_Click(object sender, EventArgs e) {
-            ResetController();
+            Reset(ChartType.BoxWhisker);
             modelChart = BoxWhisker.Create();
             controller.ChartModel = modelChart;
             viewPanel.Invalidate();
         }
 
         private void butFunnel_Click(object sender, EventArgs e) {
-            ResetController();
+            Reset(ChartType.Funnel);
             modelChart = Funnel.Create();
             controller.ChartModel = modelChart;
             viewPanel.Invalidate();
         }
 
         private void butHistogram_Click(object sender, EventArgs e) {
-            ResetController();
+            Reset(ChartType.Histogram);
             modelChart = Histogram.Create();
             controller.ChartModel = modelChart;
             viewPanel.Invalidate();
         }
 
         private void butPareto_Click(object sender, EventArgs e) {
-            ResetController();
+            Reset(ChartType.Pareto);
             modelChart = Pareto.Create();
             controller.ChartModel = modelChart;
             viewPanel.Invalidate();
         }
 
         private void butTreemap_Click(object sender, EventArgs e) {
-            ResetController();
-            treeMapChart = Treemap.Create();
-            treeMapController.TreeMapModel = treeMapChart;
+            Reset(ChartType.TreeMap);
+            modelChart = Treemap.Create();
+            controller.ChartModel = modelChart;
             viewPanel.Invalidate();
         }
     }
--- Wpf/MainWindow.xaml.cs
+++ Wpf/MainWindow.xaml.cs
@@ -1,53 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using ChartsModel = DevExpress.Charts.Model;
 using DevExpress.Xpf.Charts.ModelSupport;
 using ChartExModelSpike;
 using DevExpress.Xpf.TreeMap;
+using DevExpress.Charts.Model;
 
 namespace ChartExWpf {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    public enum ChartType {
+        BoxWhisker = 0,
+        Histogram = 1,
+        Funnel = 2,
+        Pareto = 3,
+        Sunburst = 4,
+        TreeMap = 5,
+        Waterfall = 6
+    }
     public partial class MainWindow : Window {
-        private readonly XpfChartsModelControllerFactory factory;
-        private ChartsModel.Controller controller = null;
-        private ChartsModel.Chart modelChart = null;
-
-        private readonly TreeMapModelControllerFactory treeMapFactory;
-        private TreeMapController treeMapController = null;
-        private ChartsModel.TreeMap treeMapChart = null;
+        private ModelControllerFactoryBase factory;
+        private ChartsModel.IController controller = null;
+        private ChartsModel.ChartBase modelChart = null;
 
         public MainWindow() {
             InitializeComponent();
-            factory = new XpfChartsModelControllerFactory();
-            controller = factory.CreateController();
-            treeMapFactory = new TreeMapModelControllerFactory();
-            treeMapController = (TreeMapController)treeMapFactory.CreateController();
         }
 
-        private void ResetController() {
+        private void Reset(ChartType chartType) {
             if (modelChart != null) {
                 modelChart = null;
-                controller.ChartModel = null;
-                controller = factory.CreateController();
             }
-            if (treeMapChart != null) {
-                treeMapChart = null;
-                treeMapController.ChartModel = null;
-                treeMapController = (TreeMapController)treeMapFactory.CreateController();
+            factory = CreateFactory(chartType);
+            controller = factory.CreateController();
+        }
+        ModelControllerFactoryBase CreateFactory(ChartType chartType) {
+            switch (chartType) {
+                case ChartType.BoxWhisker:
+                case ChartType.Funnel:
+                case ChartType.Histogram:
+                case ChartType.Pareto:
+                case ChartType.Waterfall:
+                    return new XpfChartsModelControllerFactory();
+                case ChartType.TreeMap:
+                case ChartType.Sunburst:
+                    return new TreeMapModelControllerFactory();
+                default:
+                    throw new ArgumentException("Unknown chart type.");
             }
         }
 
@@ -57,54 +57,49 @@ namespace ChartExWpf {
                 var renderContext = factory.CreateRenderContext(rect, viewPanel);
                 controller.RenderChart(renderContext);
             }
-            else if (treeMapChart != null) {
-                ChartsModel.ModelRect rect = new ChartsModel.ModelRect(0, 0, viewPanel.ActualWidth, viewPanel.ActualHeight);
-                var renderContext = treeMapFactory.CreateRenderContext(rect, viewPanel);
-                treeMapController.RenderChart(renderContext);
-            }
         }
 
         private void viewPanel_SizeChanged(object sender, SizeChangedEventArgs e) => RenderChart();
 
         private void butWaterfall_Click(object sender, RoutedEventArgs e) {
-            ResetController();
+            Reset(ChartType.Waterfall);
             modelChart = Waterfall.Create();
             controller.ChartModel = modelChart;
             RenderChart();
         }
 
         private void butBoxWhisker_Click(object sender, RoutedEventArgs e) {
-            ResetController();
+            Reset(ChartType.BoxWhisker);
             modelChart = BoxWhisker.Create();
             controller.ChartModel = modelChart;
             RenderChart();
         }
 
         private void butFunnel_Click(object sender, RoutedEventArgs e) {
-            ResetController();
+            Reset(ChartType.Funnel);
             modelChart = Funnel.Create();
             controller.ChartModel = modelChart;
             RenderChart();
         }
 
         private void butHistogram_Click(object sender, RoutedEventArgs e) {
-            ResetController();
+            Reset(ChartType.Histogram);
             modelChart = Histogram.Create();
             controller.ChartModel = modelChart;
             RenderChart();
         }
 
         private void butPareto_Click(object sender, RoutedEventArgs e) {
-            ResetController();
+            Reset(ChartType.Pareto);
             modelChart = Pareto.Create();
             controller.ChartModel = modelChart;
             RenderChart();
         }
 
         private void butTreemap_Click(object sender, RoutedEventArgs e) {
-            ResetController();
-            treeMapChart = Treemap.Create();
-            treeMapController.TreeMapModel = treeMapChart;
+            Reset(ChartType.TreeMap);
+            modelChart = Treemap.Create();
+            controller.ChartModel = modelChart;
             RenderChart();
         }
     }
