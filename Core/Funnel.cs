64dc03daa754f9aaeb1a8f392db14c1317c2618b
Roman Tabunnikov <roman.tabunnikov@devexpress.com>
2020-10-05 16:13:39 +0300

    Funnel based on RangeBarSeries

--- Core/Funnel.cs
+++ Core/Funnel.cs
@@ -9,31 +9,47 @@ using DevExpress.Utils;
 namespace ChartExModelSpike {
     public static class Funnel {
         public static ChartsModel.Chart Create() {
-            var chart = new ChartsModel.FunnelChart();
+            var chart = new ChartsModel.CartesianChart();
+            chart.Rotated = true;
 
             // Argument Axis
-            //chart.ArgumentAxis = new ChartsModel.Axis() {
-            //    Position = ChartsModel.AxisPosition.Bottom,
-            //    GridLinesVisible = false,
-            //    GridLinesMinorVisible = false,
-            //    TickmarksVisible = false,
-            //    Visible = true
-            //};
+            chart.ArgumentAxis = new ChartsModel.Axis() {
+                Position = ChartsModel.AxisPosition.Bottom,
+                GridLinesVisible = false,
+                GridLinesMinorVisible = false,
+                TickmarksVisible = false,
+                Reverse = true,
+                Visible = true
+            };
 
-            //chart.ArgumentAxis.Label = new ChartsModel.AxisLabel(chart.ArgumentAxis) {
-            //    EnableAntialiasing = DefaultBoolean.True,
-            //    Visible = true
-            //};
+            chart.ArgumentAxis.Label = new ChartsModel.AxisLabel(chart.ArgumentAxis) {
+                EnableAntialiasing = DefaultBoolean.True,
+                Visible = true
+            };
+
+            chart.ValueAxis = new ChartsModel.Axis() {
+                Position = ChartsModel.AxisPosition.Left,
+                GridLinesVisible = false,
+                GridLinesMinorVisible = false,
+                TickmarksVisible = false,
+                Visible = false
+            };
+
+            chart.ValueAxis.Label = new ChartsModel.AxisLabel(chart.ArgumentAxis) {
+                EnableAntialiasing = DefaultBoolean.True,
+                Visible = false
+            };
 
             // Series
-            var series = new ChartsModel.FunnelSeries() {
+            var series = new ChartsModel.RangeBarSeries() {
                 DisplayName = "Series 1",
                 LabelsVisibility = true,
-                PointShape = ChartsModel.FunnelPointShape.Rectangle
-            };
+                BarWidth = 1.0 / (1.0 + 0.25) // 25% gap
+            }; 
 
             series.DataMembers[ChartsModel.DataMemberType.Argument] = "Stage";
             series.DataMembers[ChartsModel.DataMemberType.Value] = "Amount";
+            series.DataMembers[ChartsModel.DataMemberType.Value2] = "NegAmount";
             series.DataMembers[ChartsModel.DataMemberType.Color] = "PointColor";
             series.DataSource = FunnelData.GetSampleData();
 
@@ -43,10 +59,12 @@ namespace ChartExModelSpike {
             series.Appearance.Color = new ChartsModel.ColorARGB(0xff, 0xff, 0, 0);
 
             //Data Labels
-            series.Label = new ChartsModel.SeriesLabel(series) {
-               EnableAntialiasing = DefaultBoolean.True,
-               Position = ChartsModel.SeriesLabelPosition.Center
-            };
+            // UNCOMMENT for InvalidCastException under WinForms
+            //series.Label = new ChartsModel.SeriesLabel(series) {
+            //    EnableAntialiasing = DefaultBoolean.True,
+            //    Position = ChartsModel.SeriesLabelPosition.Center
+            //};
+
             series.Appearance.LabelAppearance = new ChartsModel.SeriesLabelAppearance() {
                 LineVisible = false,
                 Border = new ChartsModel.Border() { Color = ChartsModel.ColorARGB.Transparent },
@@ -76,8 +94,79 @@ namespace ChartExModelSpike {
             chart.Titles.Add(title);
 
             ChartAppearanceHelper.SetupAppearance(chart);
+            chart.Appearance.DiagramAppearance.BorderVisible = false;
 
-            return chart;
+            return chart;            
+            
+            //var chart = new ChartsModel.FunnelChart();
+
+            //// Argument Axis
+            ////chart.ArgumentAxis = new ChartsModel.Axis() {
+            ////    Position = ChartsModel.AxisPosition.Bottom,
+            ////    GridLinesVisible = false,
+            ////    GridLinesMinorVisible = false,
+            ////    TickmarksVisible = false,
+            ////    Visible = true
+            ////};
+
+            ////chart.ArgumentAxis.Label = new ChartsModel.AxisLabel(chart.ArgumentAxis) {
+            ////    EnableAntialiasing = DefaultBoolean.True,
+            ////    Visible = true
+            ////};
+
+            //// Series
+            //var series = new ChartsModel.FunnelSeries() {
+            //    DisplayName = "Series 1",
+            //    LabelsVisibility = true,
+            //    PointShape = ChartsModel.FunnelPointShape.Rectangle
+            //};
+
+            //series.DataMembers[ChartsModel.DataMemberType.Argument] = "Stage";
+            //series.DataMembers[ChartsModel.DataMemberType.Value] = "Amount";
+            //series.DataMembers[ChartsModel.DataMemberType.Color] = "PointColor";
+            //series.DataSource = FunnelData.GetSampleData();
+
+            ////series.Appearance = new ChartsModel.SeriesAppearance() { Color = new ChartsModel.ColorARGB(0xff, 0x44, 0x72, 0xc4) };
+            //series.Appearance = new ChartsModel.SeriesAppearance();
+            //series.Appearance.FillStyle = new ChartsModel.FillStyle() { FillMode = ChartsModel.FillMode.Solid };
+            //series.Appearance.Color = new ChartsModel.ColorARGB(0xff, 0xff, 0, 0);
+
+            ////Data Labels
+            //series.Label = new ChartsModel.SeriesLabel(series) {
+            //   EnableAntialiasing = DefaultBoolean.True,
+            //   Position = ChartsModel.SeriesLabelPosition.Center
+            //};
+            //series.Appearance.LabelAppearance = new ChartsModel.SeriesLabelAppearance() {
+            //    LineVisible = false,
+            //    Border = new ChartsModel.Border() { Color = ChartsModel.ColorARGB.Transparent },
+            //    TextColor = new ChartsModel.ColorARGB(0xff, 0xff, 0xff, 0xff),
+            //    BackColor = ChartsModel.ColorARGB.Transparent
+            //};
+
+            //if (series is ChartsModel.ISupportTransparencySeries seriesWithTransparency)
+            //    seriesWithTransparency.Transparency = 0;
+
+            //chart.Series.Add(series);
+
+            //// Legend
+            //var legend = new ChartsModel.Legend {
+            //    EnableAntialiasing = DefaultBoolean.True,
+            //    LegendPosition = ChartsModel.LegendPosition.Top,
+            //    Orientation = ChartsModel.LegendOrientation.Horizontal,
+            //    Overlay = false
+            //};
+            //chart.Legend = legend;
+
+            //// Title
+            //var title = new ChartsModel.ChartTitle {
+            //    EnableAntialiasing = DefaultBoolean.True,
+            //    Lines = new string[] { "Sales Pipeline" }
+            //};
+            //chart.Titles.Add(title);
+
+            //ChartAppearanceHelper.SetupAppearance(chart);
+
+            //return chart;
         }
     }
 
@@ -90,6 +179,7 @@ namespace ChartExModelSpike {
 
         public string Stage { get; private set; }
         public double Amount { get; private set; }
+        public double NegAmount => -Amount;
         public int? PointColor { get; set; }
 
         public static List<FunnelData> GetSampleData() {
